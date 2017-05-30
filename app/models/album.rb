1a7cb11fd227f544913c42569f50f550ac6c513c
Brian J. Cardiff <bcardiff@manas.tech>
2017-05-30 14:02:43 -0300

    Scaffold albums with some validations

---
+++ app/models/album.rb
@@ -0,0 +1,11 @@
+class Album < ApplicationRecord
+  validates :title, presence: true
+  validate :year_range
+
+  def year_range
+    if year.present?
+      errors.add(:year, "can't be that old") if year < 1970
+      errors.add(:year, "can't be in the future") if year > Date.today.year
+    end
+  end
+end
